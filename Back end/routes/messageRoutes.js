09caf42f1f10d9c42e452f20293f7290c1c6b4ab
Aldin Cimic <99726382+aciimicc@users.noreply.github.com>
2025-04-08 15:48:55 +0200

    Delete Back end directory

--- Back end/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { sendMessage } = require("../controllers/messageController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.post("/send", authMiddleware, sendMessage);
-
-module.exports = router;
