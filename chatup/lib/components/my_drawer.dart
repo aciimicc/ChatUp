bd7be306e6e820c365307663f7d288fc77415154
BekirC113 <220302120@student.ius.edu.ba>
2025-05-20 17:58:37 +0200

    Adjusting services

--- chatup/lib/components/my_drawer.dart
+++ chatup/lib/components/my_drawer.dart
@@ -1,24 +1,88 @@
 import 'package:flutter/material.dart';
+import '../auth/auth/_service.dart';
+import '../pages/settings_page.dart';
+
+void logout(){
+//get auth service
+final auth = AuthService();
+auth.signOut();
+}
+
 
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        actions: [
-          // logout button
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
+ Widget build(BuildContext context) {
+    return Drawer (
+        backgroundColor: Theme.off(context).colorScheme.background,
+        child: Column (
+            mainAxisAligment: MainAxisAligment.spaceBetween,
+            children: (
+          Column(      
+         // logo
+          DrawerHeader(
+          child: Center(
+          child: Icon(
+            Icons.message, 
+            color:Theme.of(context).colorScheme.primary
+            size: 40,
+            ),
           ),
-        ],
-      ),
-      drawer: const MyDrawer(),
-    );
+        ),
+    )
+   
+    )
+        
+        
+         //home list title
+         Padding(
+        padding:const EdgeInsets.only(left: 25.0, bottom: 25),
+        child:ListTile(
+        title: const Text("H O M E")
+        leading:const Icon(Icons.home),
+        onTap:() {
+        // pop the drawer
+        Navigator.pop(context);
+        },
+         ),
+        ),
+       ),
+
+          //settings list title
+         Padding(
+        padding:const EdgeInsets.only(left: 25.0),
+        child:ListTile(
+        title:const Text("S E T T I N G S")
+        leading:const Icon(Icons.home),
+        onTap:() {
+            // pop the drawer
+            Navigator.pop(context);
+
+        // navigate to settings page
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder:(context) => const SettingsPage(),
+                )
+            )
+        },
+         ),
+        ),
+
+
+        //logout list title
+         Padding(
+        padding:const EdgeInsets.only(left: 25.0),
+        child:ListTile(
+        title: const Text("L O G O U T")
+        leading:const Icon(Icons.home),
+        onTap: logout,
+         ),
+        ),
+
+    
   }
+}
 }
--- chatup/lib/pages/home_page.dart
+++ chatup/lib/pages/home_page.dart
@@ -1,73 +1,23 @@
+import 'package:chatapptute/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import '../components/my_drawer.dart';
 
-class MyDrawer extends StatelessWidget {
-  const MyDrawer({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
+  void logout() {
+    // get auth service
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Drawer (
-        backgroundColor: Theme.off(context).colorScheme.background,
-        child: Column (
-            mainAxisAligment: MainAxisAligment.spaceBetween,
-            children: (
-          Column(      
-         // logo
-          DrawerHeader(
-          child: Center(
-          child: Icon(
-            Icons.message, 
-            color:Theme.of(context).colorScheme.primary
-            size: 40,
-            ),
-          ),
-        ),
-    )
-   // logo
-          DrawerHeader(
-          child: Center(
-          child: Icon(
-            Icons.message, 
-            color:Theme.of(context).colorScheme.primary
-            size: 40,
-            ),
-          ),
-        ),
-    )
-        
-        
-         //home list title
-         Padding(
-        padding:const EdgeInsets.only(left: 25.0, bottom: 25),
-        child:ListTile(
-        title: Text("H O M E")
-        leading:Icon(Icons.home),
-        onTap:() {},
-         ),
-        ),
-       ),
-
-          //settings list title
-         Padding(
-        padding:const EdgeInsets.only(left: 25.0),
-        child:ListTile(
-        title: Text("S E T T I N G S")
-        leading:Icon(Icons.home),
-        onTap:() {},
-         ),
-        ),
-
-
-        //logout list title
-         Padding(
-        padding:const EdgeInsets.only(left: 25.0),
-        child:ListTile(
-        title: Text("L O G O U T")
-        leading:Icon(Icons.home),
-        onTap:() {},
-         ),
-        ),
-
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ), // AppBar
+      drawer: MyDrawer(),
+    ); // Scaffold
   }
 }
